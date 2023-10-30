8061545c9d177f2556c4d65847f082430b98ad3a
joelnyongesa <joelnyongesa148@gmail.com>
2023-10-30 18:57:02 +0300

    Update models to have them running, update serializer mixin and minor fixes

--- server/models.py
+++ server/models.py
@@ -1,42 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy_serializer import SerializerMixin
 
 
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__="users"
+
+    serialize_rules = ("-game_entries.user", "-game_reviews.user", )
+
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullablle=False, unique=True)
-    _password_hash = db.Column(db.String())
+    email = db.Column(db.String, nullable=False, unique=True)
+    # _password_hash = db.Column(db.String())
 
     game_entries = db.relationship('GameEntry', backref="user" )
-    game_reviews = db.relationship('GameEntry', backref="review" )
+    game_reviews = db.relationship('GameReview', backref="user" )
 
     def __repr__(self):
         return f'<User: {self.username}>'
 
 
 
-class GameEntry(db.Model):
+class GameEntry(db.Model, SerializerMixin):
     __tablename__="game_entries"
+
+    serialize_rules = ('-genres.game_entries', '-user.game_entries', '-game_genres.game_entries', )
+
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(), nullable=False)
-    platform = db.Column(db.String(), nullablle=False)
-    description = db.Colum(db.String(100))
-    user_id = db.Column(db.Integer(), db. ForeignKey('users.id'))
+    platform = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(100))
 
-    genres = db.relationship('Genre', secondary='game_genre', back_populates ='games')
+    user_id = db.Column(db.Integer(), db. ForeignKey('users.id'))
+    # genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))
+
+    genres = db.relationship('GameGenre', backref='game_entry')
+    game_reviews = db.relationship('GameReview', backref='game_entry')
 
     def __repr__(self):
         return f'Game: {self.title}, Platform: {self.platform}'
 
 
-class GameReview(db.Model):
+class GameReview(db.Model, SerializerMixin):
     __tablename__='game_reviews'
+
+    serialize_rules = ('-user.game_reviews', '-game_entries.game_reviews', )
 
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer())
@@ -47,24 +59,31 @@ class GameReview(db.Model):
     game_entry_id = db.Column(db.Integer(), db.ForeignKey('game_entries.id'))
 
     def __repr__(self):
-        return
+        return f'<Review: \n Score: {self.rating}, Comment: {self.comment}>'
 
 
 
 
-class Genre(db.Model):
+class Genre(db.Model, SerializerMixin):
     __tablename__="genres"
+
+    serialize_rules = ('-game_entries.genres', '-game_genres.genres', )
+
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String, nullable=False)
 
-    games = db.relationship('GameEntry', secondary='game_genre', back_populates='genres')
+    game_entries = db.relationship('GameGenre', backref='genre')
 
     def __repr__(self):
-        return f'Genre: {self.name}'
+        return f'<Genre: {self.name}>'
 
 
 
-class GameGenre(db.Model):
-    __tablename__ = "game_genre"
-    game_entry_id = db.Column(db.Integer(), db.ForeignKey('game_entries.id'), primary_key=True)
-    genre_id = db.Column(db.Integer(), db.ForeignKey('genres.id'), primary_key=True)
+class GameGenre(db.Model, SerializerMixin):
+    __tablename__ = "game_genres"
+
+    serialize_rules = ('-game_entries.game_genres', '-genres.game_genres', )
+
+    id=db.Column(db.Integer, primary_key=True)
+    game_entry_id = db.Column(db.Integer(), db.ForeignKey('game_entries.id'))
+    genre_id = db.Column(db.Integer(), db.ForeignKey('genres.id'))
